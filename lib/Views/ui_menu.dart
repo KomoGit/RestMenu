cb9cfabc74943ab5dcb2507bad9d5bc0991475f3
James <entreur@gmail.com>
2023-01-27 20:30:52 +0400

    Finally, the backend is working with front-end.

--- lib/Views/ui_menu.dart
+++ lib/Views/ui_menu.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:restmenu/Logic/API/api_category.dart';
+import 'package:restmenu/Logic/API/api_menu.dart';
 import 'package:restmenu/Logic/Debug/de_logger.dart';
 import 'package:restmenu/Model/model_category.dart';
 import 'package:restmenu/Model/model_menu.dart';
@@ -14,7 +15,7 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   List<Category>? cat;
   List<MenuItem>?
-      menu; //It is possible that these two items are null when they are not being called from getData();
+      menuItem; //It is possible that these two items are null when they are not being called from getData();
   int? categoryLength;
   var isLoaded = false;
 
@@ -33,15 +34,13 @@ class _HomeState extends State<Home> {
   //Current issue we are facing right now. The data which is not supposed to be null is coming back to us as null.
   //I have no current understanding as why?
   getData() async {
-    final List<Category> cat = await CategoryAPI().fromRecordsToModels();
-    // final List<MenuItem> menuItem = await MenuAPI().fromRecordsToModels();
-    if (cat.isNotEmpty) {
+    /*final List<Category>*/ cat = await CategoryAPI()
+        .fromRecordsToModels(); //I am an idiot. Let it be known.
+    /* final List<MenuItem>*/ menuItem = await MenuAPI().fromRecordsToModels();
+    if (cat!.isNotEmpty) {
       setState(() {
-        categoryLength = cat.length;
+        categoryLength = cat!.length;
         isLoaded = true;
-        for (var i = 0; i < cat.length; i++) {
-          Logger.logOut(cat[i].title);
-        }
       });
     }
   }
@@ -60,7 +59,7 @@ class _HomeState extends State<Home> {
             itemBuilder: (context, index) {
               return Center(
                   child: Text(cat != null
-                      ? cat![index - 1].title
+                      ? cat![index].title
                       : "Warning, null value!"));
             }),
       ),
