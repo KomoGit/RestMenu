71abe938fbc178c4033585fc3c46ab1ecb22ad3b
James <entreur@gmail.com>
2023-01-27 20:40:57 +0400

    Instead of making pb, create one pb use twice.

--- lib/Logic/API/api_category.dart
+++ lib/Logic/API/api_category.dart
@@ -2,10 +2,8 @@ import 'package:pocketbase/pocketbase.dart';
 import 'package:restmenu/Model/model_category.dart';
 
 class CategoryAPI {
-  final PocketBase _pb = PocketBase("http://127.0.0.1:8090");
-
-  Future<List<Category>> fromRecordsToModels() async {
-    var rawData = await _pb.collection("category").getFullList();
+  Future<List<Category>> fromRecordsToModels(PocketBase pb) async {
+    var rawData = await pb.collection("category").getFullList();
     List<RecordModel> listOfCategory = rawData;
     List<Category> categories = [];
     for (RecordModel model in listOfCategory) {
--- lib/Logic/API/api_menu.dart
+++ lib/Logic/API/api_menu.dart
@@ -2,10 +2,8 @@ import 'package:pocketbase/pocketbase.dart';
 import 'package:restmenu/Model/model_menu.dart';
 
 class MenuAPI {
-  final PocketBase _pb = PocketBase("http://127.0.0.1:8090");
-
-  Future<List<MenuItem>> fromRecordsToModels() async {
-    var rawData = await _pb.collection("menuitem").getFullList();
+  Future<List<MenuItem>> fromRecordsToModels(PocketBase pb) async {
+    var rawData = await pb.collection("menuitem").getFullList();
     List<RecordModel> listOfMenuItem = rawData;
     List<MenuItem> categories = [];
     for (RecordModel model in listOfMenuItem) {
