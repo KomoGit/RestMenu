2bc9f35c62d603895d851a94202a15aad54bbce8
James <entreur@gmail.com>
2023-01-27 20:32:18 +0400

    Code cleanup.

--- lib/Logic/API/api_menu.dart
+++ lib/Logic/API/api_menu.dart
@@ -4,11 +4,6 @@ import 'package:restmenu/Model/model_menu.dart';
 class MenuAPI {
   final PocketBase _pb = PocketBase("http://127.0.0.1:8090");
 
-  // Future<List<RecordModel>> getAllCategories() async {
-  //   final data = _pb.collection("MenuItem").getFullList();
-  //   return data;
-  // }
-
   Future<List<MenuItem>> fromRecordsToModels() async {
     var rawData = await _pb.collection("menuitem").getFullList();
     List<RecordModel> listOfMenuItem = rawData;
