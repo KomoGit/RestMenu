40e621b92cbec5a60e5d9909a8abb7e5bd134191
James <entreur@gmail.com>
2023-01-27 17:12:02 +0400

    Restructure for MenuModel

--- lib/Model/model_menuitems.dart
+++ lib/Model/model_menuitems.dart
@@ -3,16 +3,22 @@ import 'package:pocketbase/pocketbase.dart';
 class MenuItem {
   bool active;
   String title;
+  String desc;
+  double price;
   String img;
   String imgUrl;
 
-  MenuItem(this.active, this.title, this.img, this.imgUrl);
+  MenuItem(
+      this.active, this.title, this.desc, this.price, this.img, this.imgUrl);
 
   static MenuItem fromModel(RecordModel model) {
     bool active = model.getBoolValue('active');
     String title = model.getStringValue('title');
+    String desc = model.getStringValue('desc');
     String imgUrl = model.getStringValue('imgUrl');
     String img = model.getStringValue('img');
-    return MenuItem(active, title, img, imgUrl);
+    double price = model.getDoubleValue('price');
+
+    return MenuItem(active, title, desc, price, img, imgUrl);
   }
 }
