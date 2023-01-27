9a6ea7074c542c8891407103d80583552f4e31bc
James <entreur@gmail.com>
2023-01-27 17:10:52 +0400

    Rename

--- lib/Logic/API/api_category.dart
+++ lib/Logic/API/api_category.dart
@@ -1,16 +1,14 @@
 import 'package:pocketbase/pocketbase.dart';
 import 'package:restmenu/Model/model_category.dart';
 
-class MyPocketBase {
+class CategoryAPI {
   final PocketBase _pb = PocketBase("http://127.0.0.1:8090");
 
-  // Returns a list of record model.....Kinda tricky. How do i process this?
   Future<List<RecordModel>> getAllCategories() async {
     final data = _pb.collection("category").getFullList();
     return data;
   }
 
-  //Same as above my dear. How do we parse this into a model?
   Future<List<RecordModel>> getAllMenuItems() async {
     final data = _pb.collection("menuitem").getFullList();
     return data;
