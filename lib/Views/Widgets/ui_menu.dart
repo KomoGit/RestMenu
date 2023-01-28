7e59b6c711fe421d03e58d79fbf026b1780822d1
James <entreur@gmail.com>
2023-01-28 12:26:58 +0400

    Menu is all good, needs inkwell.

--- lib/Views/Widgets/ui_menu.dart
+++ lib/Views/Widgets/ui_menu.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MenuView extends StatelessWidget {
   final String title;
@@ -17,6 +18,38 @@ class MenuView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Align(
+      alignment: Alignment.centerLeft,
+      child: Container(
+        margin: const EdgeInsets.all(8.0),
+        child: Card(
+          shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(8.0))),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center, //was .stretch
+            children: [
+              ClipRRect(
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(8.0),
+                  topRight: Radius.circular(8.0),
+                ),
+                child: Image.network(imgUrl,
+                    width: 300, height: 150, fit: BoxFit.fill),
+              ),
+              ListTile(
+                title: Text(
+                  title,
+                  style: GoogleFonts.montserrat(),
+                ),
+                subtitle: Text(
+                  price.toString(),
+                  style: GoogleFonts.montserrat(color: Colors.yellow),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
