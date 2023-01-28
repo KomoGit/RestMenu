465d755cec8a745bf11d2c59f2dd21a7b4e1ea40
James <entreur@gmail.com>
2023-01-28 14:10:46 +0400

    Removed img and added category.

--- lib/Model/model_menu.dart
+++ lib/Model/model_menu.dart
@@ -1,29 +1,24 @@
 import 'package:pocketbase/pocketbase.dart';
 
-/*
-TODO: We need to add a variable here that will also contain category,
- and perhaps get rid of String img as we don't use it to represent anything.
- */
 class MenuItem {
   bool active;
   String title;
   String desc;
-  String img;
+  String category;
   String imgUrl;
-
   double price;
 
-  MenuItem(
-      this.active, this.title, this.desc, this.price, this.img, this.imgUrl);
+  MenuItem(this.active, this.title, this.desc, this.price, this.category,
+      this.imgUrl);
 
   static MenuItem fromModel(RecordModel model) {
     bool active = model.getBoolValue('active');
     String title = model.getStringValue('title');
     String desc = model.getStringValue('desc');
     String imgUrl = model.getStringValue('imgUrl');
-    String img = model.getStringValue('img');
+    String category = model.getStringValue('category');
     double price = model.getDoubleValue('price');
 
-    return MenuItem(active, title, desc, price, img, imgUrl);
+    return MenuItem(active, title, desc, price, category, imgUrl);
   }
 }
