1187effabdbcd83a20cdf3181da5fd41ace3e24d
James <entreur@gmail.com>
2023-01-28 17:55:07 +0400

    Code cleanup.

--- lib/Views/Widgets/ui_category.dart
+++ lib/Views/Widgets/ui_category.dart
@@ -8,34 +8,34 @@ class CategoryView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Align(
-      alignment: Alignment.centerLeft,
-      child: Container(
-        margin: const EdgeInsets.all(8.0),
-        child: Card(
-          shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(Radius.circular(8.0))),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center, //was .stretch
-            children: [
-              ClipRRect(
-                borderRadius: const BorderRadius.only(
-                  topLeft: Radius.circular(8.0),
-                  topRight: Radius.circular(8.0),
-                ),
-                child: Image.network(imgUrl,
-                    width: 300, height: 150, fit: BoxFit.fill),
+    // return Container(
+    //   color: Colors.black,
+    // );
+    return Container(
+      margin: const EdgeInsets.all(8.0),
+      child: Card(
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(8.0))),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center, //was .stretch
+          children: [
+            ClipRRect(
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(8.0),
+                topRight: Radius.circular(8.0),
               ),
-              ListTile(
-                title: Center(
-                  child: Text(
-                    title,
-                    style: GoogleFonts.montserrat(),
-                  ),
+              child: Image.network(imgUrl,
+                  width: 300, height: 150, fit: BoxFit.fill),
+            ),
+            ListTile(
+              title: Center(
+                child: Text(
+                  title,
+                  style: GoogleFonts.montserrat(),
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
