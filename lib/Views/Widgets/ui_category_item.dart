99f9b9a14b5e223dc9343214d05329c3eb171b45
James <entreur@gmail.com>
2023-01-29 18:56:36 +0400

    Renaming Category.

--- lib/Views/Widgets/ui_category_item.dart
+++ lib/Views/Widgets/ui_category_item.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:restmenu/Model/model_menu.dart';
 
+/*Filtering is what we have left for this part of the project. 
+Once that is done we only need to implement a button and we are golden*/
+
 class CategoryView extends StatelessWidget {
   final List<MenuItem> menuItem;
   final int index;
@@ -20,22 +23,26 @@ class CategoryView extends StatelessWidget {
         const Padding(padding: EdgeInsets.all(10)),
         //Image has anti-alliasing issues.
         SizedBox(
-          width: 35,
-          height: 35,
-          child: Center(child: Image.network(imgUrl)),
-        ),
+            width: 50,
+            height: 50,
+            child: Center(
+                child: Image.network(
+              filterQuality: FilterQuality.high,
+              imgUrl,
+              isAntiAlias: true,
+            ))),
         const Padding(padding: EdgeInsets.all(10)),
         ExpansionTile(title: Text(title), children: [
-          const Padding(padding: EdgeInsets.all(5)),
+          const Padding(
+            padding: EdgeInsets.all(5),
+          ),
           SizedBox(
             height: MediaQuery.of(context).size.height - 600,
             child: ListView.builder(
                 itemCount: menuItem.length,
                 itemBuilder: (context, index) {
                   return Center(
-                    child: Text(
-                      menuItem[index].title,
-                    ),
+                    child: Text(menuItem[index].title),
                   );
                 }),
           ),
