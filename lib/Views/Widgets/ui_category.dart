122c55ca2f93c653de119bca761fa375ae016030
James <entreur@gmail.com>
2023-01-29 12:47:11 +0400

    Coming closer to what I had in mind.

--- lib/Views/Widgets/ui_category.dart
+++ lib/Views/Widgets/ui_category.dart
@@ -1,43 +1,45 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:restmenu/Model/model_menu.dart';
+import 'package:restmenu/Views/Widgets/ui_menu.dart';
 
 class CategoryView extends StatelessWidget {
+  final List<MenuItem> menuItem;
   final String title;
   final String imgUrl;
-  const CategoryView({required this.title, required this.imgUrl, super.key});
+  const CategoryView(
+      {required this.title,
+      required this.imgUrl,
+      required this.menuItem,
+      super.key});
 
   @override
   Widget build(BuildContext context) {
-    // return Container(
-    //   color: Colors.black,
-    // );
-    return Container(
-      margin: const EdgeInsets.all(8.0),
-      child: Card(
-        shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(8.0))),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center, //was .stretch
+    return Column(
+      children: [
+        const Padding(padding: EdgeInsets.all(10)),
+        //Image has anti-alliasing issues.
+        SizedBox(
+          width: 35,
+          height: 35,
+          child: Center(child: Image.network(imgUrl)),
+        ),
+        const Padding(padding: EdgeInsets.all(10)),
+        ExpansionTile(
+          title: Text(title),
           children: [
-            ClipRRect(
-              borderRadius: const BorderRadius.only(
-                topLeft: Radius.circular(8.0),
-                topRight: Radius.circular(8.0),
-              ),
-              child: Image.network(imgUrl,
-                  width: 300, height: 150, fit: BoxFit.fill),
-            ),
-            ListTile(
-              title: Center(
-                child: Text(
-                  title,
-                  style: GoogleFonts.montserrat(),
-                ),
-              ),
-            ),
+            ListView.builder(
+                itemCount: menuItem.length,
+                itemBuilder: (context, index) {
+                  /*MenuView(
+                      title: menuItem[index].title,
+                      desc: menuItem[index].desc,
+                      category: menuItem[index].category,
+                      imgUrl: menuItem[index].imgUrl,
+                      price: menuItem[index].price);*/
+                })
           ],
         ),
-      ),
+      ],
     );
   }
 }
