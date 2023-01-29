0d7832ef23b91aa943bcff06e30ef83534d05f47
James <entreur@gmail.com>
2023-01-29 14:47:05 +0400

    Category works. Needs filters.

--- lib/Views/Widgets/ui_category.dart
+++ lib/Views/Widgets/ui_category.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:restmenu/Model/model_menu.dart';
-import 'package:restmenu/Views/Widgets/ui_menu.dart';
 
 class CategoryView extends StatelessWidget {
   final List<MenuItem> menuItem;
+  final int index;
   final String title;
   final String imgUrl;
   const CategoryView(
       {required this.title,
+      required this.index,
       required this.imgUrl,
       required this.menuItem,
       super.key});
@@ -24,21 +25,21 @@ class CategoryView extends StatelessWidget {
           child: Center(child: Image.network(imgUrl)),
         ),
         const Padding(padding: EdgeInsets.all(10)),
-        ExpansionTile(
-          title: Text(title),
-          children: [
-            ListView.builder(
+        ExpansionTile(title: Text(title), children: [
+          const Padding(padding: EdgeInsets.all(5)),
+          SizedBox(
+            height: MediaQuery.of(context).size.height - 600,
+            child: ListView.builder(
                 itemCount: menuItem.length,
                 itemBuilder: (context, index) {
-                  /*MenuView(
-                      title: menuItem[index].title,
-                      desc: menuItem[index].desc,
-                      category: menuItem[index].category,
-                      imgUrl: menuItem[index].imgUrl,
-                      price: menuItem[index].price);*/
-                })
-          ],
-        ),
+                  return Center(
+                    child: Text(
+                      menuItem[index].title,
+                    ),
+                  );
+                }),
+          ),
+        ]),
       ],
     );
   }
