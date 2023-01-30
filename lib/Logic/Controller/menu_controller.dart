4159f2982302502695a031c571c37e272e932a5c
James <entreur@gmail.com>
2023-01-30 17:48:51 +0400

    Code cleanup.

--- lib/Logic/Controller/menu_controller.dart
+++ lib/Logic/Controller/menu_controller.dart
@@ -12,18 +12,4 @@ class MenuAPI {
     }
     return categories;
   }
-
-  //DO NOT DELETE!
-  // Future<List<MenuItem>> filterRecord(
-  //     PocketBase pb, String filterOptions) async {
-  //   var rawData = await pb
-  //       .collection("menuitem")
-  //       .getFullList(filter: 'category = $filterOptions');
-  //   List<RecordModel> listOfMenuItem = rawData;
-  //   List<MenuItem> categories = [];
-  //   for (RecordModel model in listOfMenuItem) {
-  //     categories.add(MenuItem.fromModel(model));
-  //   }
-  //   return categories;
-  // }
 }
