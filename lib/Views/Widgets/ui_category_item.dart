cf690481c6d8118f8e9a797e6bb3e3a09a05a42c
James <entreur@gmail.com>
2023-01-30 15:59:02 +0400

    TODO: Ensure empty categories don't get created.

--- lib/Views/Widgets/ui_category_item.dart
+++ lib/Views/Widgets/ui_category_item.dart
@@ -3,7 +3,7 @@ import 'package:restmenu/Model/model_menu.dart';
 
 /*Filtering is what we have left for this part of the project. 
 Once that is done we only need to implement a button and we are golden*/
-
+//TODO: IMPLEMENT - If category is empty, do not generate.
 class CategoryView extends StatelessWidget {
   final List<MenuItem> menuItem;
   final int index;
